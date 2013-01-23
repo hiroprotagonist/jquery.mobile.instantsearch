68289e7a191824c2f427031af558e4bad9fafff5
Stefan Gebhardt <stefan.gebhardt@finux.net>
2013-01-23 09:39:33 +0100

    Even Better

--- jquery.mobile.instantsearch.js
+++ jquery.mobile.instantsearch.js
@@ -1,60 +1,80 @@
 /*
- * jquery.mobile.actionsheet v1
+ * jquery.mobile.actionsheet v2
  *
  * Copyright (c) 2011, Stefan Gebhardt
  * Dual licensed under the MIT and GPL Version 2 licenses.
  * 
  * Date: 2011-05-03 17:11:00 (Tue, 3 May 2011)
- * Revision: 1.1
  */
-(function($, window, undefined) {
+(function ($, window, undefined) {
 	$.widget("mobile.instantsearch", $.mobile.widget, {
-		si: undefined,	//search inupt
-		rd: undefined,	//result div
+		inputField: undefined, //Search inupt
+		resultDisplay: undefined, //Result div
 		timeout: undefined,
 		options: {
-			delay: 500
+			delay: 500,
+			instantsearchResultboxid: null
 		},
-		_init: function() {
-			this.rd= $("<div>");
-			this.element.after(this.rd);
-			this.si= this.element.find(":jqmData(type='search')");
-			this.element.submit(function() {
+		_init: function () {
+			if (this.options.instantsearchResultboxid !== null) {
+				this.resultDisplay = $('#' + this.options.instantsearchResultboxid);
+			} else {
+				this.resultDisplay = $('<div>');
+				this.element.after(this.resultDisplay);
+			}
+
+			this.element.submit(function () {
 				return false;
 			});
-			this.si.bind('keyup', $.proxy(this.delaysearch, this));
+			this.element.bind('search', $.proxy(this.delaysearch, this));
+
+			this.inputField = this.element.find(':jqmData(type="search")');
+			this.inputField.bind('keyup', $.proxy(this.delaysearch, this));
 		},
-		delaysearch: function() {
-			if(typeof this.timeout !== 'undefined') {
+		delaysearch: function () {
+			if (typeof this.timeout !== 'undefined') {
 				window.clearTimeout(this.timeout);
 			}
-			this.timeout= window.setTimeout($.proxy(this.submit, this), this.options.delay);
+			this.timeout = window.setTimeout($.proxy(this.submit, this), this.options.delay);
 		},
-		submit: function() {
+		submit: function () {
 			$.ajax({
 				url: this.element.attr('action'),
 				context: this,
 				dataType: 'html',
 				type: this.element.attr('method'),
 				data: this.element.serialize(),
-				beforeSend: function() {
-					this.si.addClass('loading');
+				beforeSend: function () {
+					this.inputField.addClass('loading');
 				},
-				complete: function() {
-					this.si.removeClass('loading');
+				complete: function () {
+					this.inputField.removeClass('loading');
 				},
-				success: function(html) {
-					var ul= $(html);
-					this.rd.html(ul);
-					this.rd.children('ul').listview();
+				success: function (html) {
+					var content = $(html);
+					this.resultDisplay.html(content);
+					// Enhance the content if necessary
+					if ( typeof(content.attr('data-role')) !== 'undefined' ) {
+						var pluginName = content.attr('data-role');
+						content[pluginName]();
+					}
+					
+					// Enhance the children of content if necessary
+					$.each(content.find('[data-role]'), function() {
+						var item = $(this)
+						  , pluginName = item.attr('data-role');
+						if ( typeof item[pluginName] === 'function' ) { 
+							item[pluginName]();
+						}
+					});
 				}
 			});
 		}
 	});
 
-	$(":jqmData(role='page')").live("pageinit", function() {
-		$(":jqmData(role='instantsearch')", this).each(function() {
+	$(":jqmData(role='page')").live("pageinit", function () {
+		$(":jqmData(role='instantsearch')", this).each(function () {
 			$(this).instantsearch();
 		});
 	});
-}) (jQuery, this);
+})(jQuery, this);
